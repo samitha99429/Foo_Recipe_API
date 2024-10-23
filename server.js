a7ba45b21cdb1222ce23b7cb65a54165f4442593
samitha senevirathne <88784541+samitha99429@users.noreply.github.com>
2024-10-23 19:19:17 +0530

    done

--- server.js
+++ server.js
@@ -11,9 +11,23 @@ dotenv.config();
 connectDB();
 
 const app = express();
+// CORS middleware to handle cross-origin requests
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://food-recipe-client-38kigbhwn-samithas-projects-66fa87b4.vercel.app');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', 'true');
+
+  // Handle preflight OPTIONS request
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200); // Send OK status for preflight requests
+  }
+
+  next();
+});
 
 app.use(cors({
-  origin: ['https://food-recipe-client-38kigbhwn-samithas-projects-66fa87b4.vercel.app'], 
+  origin: '*', 
   methods: ['GET', 'POST', 'DELETE', 'OPTIONS'], 
   credentials: true, 
 }));
