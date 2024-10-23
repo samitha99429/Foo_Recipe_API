8b794867b10d6cd81968e72a97309a0b9fc794fa
samitha senevirathne <88784541+samitha99429@users.noreply.github.com>
2024-10-23 17:59:09 +0530

    done

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,14 +4,6 @@ const router = express.Router();
 
 
 
-router.get("/", (req, res) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");  
-    res.setHeader("Access-Control-Allow-Credentials", "true");
-    res.setHeader("Access-Control-Max-Age", "1800");  
-    res.setHeader("Access-Control-Allow-Headers", "content-type");
-    res.setHeader("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS");
-    res.send('API is running...');
-  });
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
