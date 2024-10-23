e652a20eb64d90ed098ce992c0153842613e135c
samitha senevirathne <88784541+samitha99429@users.noreply.github.com>
2024-10-23 17:20:41 +0530

    done

--- server.js
+++ server.js
@@ -3,47 +3,40 @@ const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const authRoutes = require('./routes/authRoutes');
 const recipeRoutes = require('./routes/recipeRoutes');
-// const cors = require('cors');
+const cors = require('cors');
 
-// Load environment variables
+
 dotenv.config();
 
-// Connect to MongoDB
+
 connectDB();
 
 const app = express();
 
 
-const cors=require("cors");
-const corsOptions ={
-   origin:'*', 
-   credentials:true,            //access-control-allow-credentials:true
-   optionSuccessStatus:200,
-}
 
 app.use(cors(corsOptions));
 
-// Set up CORS
+
 app.use(cors({
-  origin: ['https://food-recipe-client-dfjrgg2qf-samithas-projects-66fa87b4.vercel.app'], // Allow your client URL
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allow OPTIONS for preflight
+  origin: ['https://food-recipe-client-dfjrgg2qf-samithas-projects-66fa87b4.vercel.app'], 
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], 
   credentials: true,
-  allowedHeaders: ['Content-Type', 'Authorization'], // Include allowed headers
+  allowedHeaders: ['Content-Type', 'Authorization'], 
 }));
 
 
 
-// Middleware for parsing JSON requests
 app.use(express.json());
 
-// Manually handle preflight requests if necessary
-// app.options('*', cors()); 
 
-// Route definitions
+app.options('*', cors()); 
+
+
 app.use('/api/auth', authRoutes);
 app.use('/api/recipes', recipeRoutes);
 
-// Start the server
+
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
